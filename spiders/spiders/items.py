652225d1de307e51c1ed826fe35c507ece101d1d
Henk Griffioen <hgrif@users.noreply.github.com>
2015-06-09 20:39:00 +0200

    Initial commit

---
+++ spiders/spiders/items.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NameItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    name = scrapy.Field()
+    name_type = scrapy.Field()
+    gender = scrapy.Field()
+    first_count = scrapy.Field()
+    first_percent = scrapy.Field()
+    follow_count = scrapy.Field()
+    follow_percent = scrapy.Field()
+    year = scrapy.Field()
+    value = scrapy.Field()
+    step_size = scrapy.Field()
+    step_total_value = scrapy.Field()
+    step_total_approximation = scrapy.Field()
+    approximation = scrapy.Field()
+
+
+
---
+++ spiders/spiders/settings.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for spiders project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'spiders'
+
+SPIDER_MODULES = ['spiders.spiders']
+NEWSPIDER_MODULE = 'spiders.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'spiders (+http://www.yourdomain.com)'
+
+#LOG_LEVEL = 'DEBUG'
+CONCURRENT_REQUESTS = 1
+DOWNLOAD_DELAY = 0.1
+LOG_FILE = 'log.log'
